056497e6dc4e41c299d1cbf43b7c239b3fe78ad5
tuhin <myid.tuhin123@gmail.com>
2016-07-29 09:48:14 +0530

    some java basics

---
+++ java-preps/src/com/tuhin/javapreps/misc/RelationalOperators.java
@@ -0,0 +1,20 @@
+package com.tuhin.javapreps.misc;
+
+/**
+ * Created by tuhinmandal on 29/07/16.
+ */
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        if(a > b & b > c) {
+            System.out.println("okay");
+        } else {
+            System.out.println("not okay");
+        }
+
+    }
+}
